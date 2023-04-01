55bdfc015539bb1766e2430a2f5ac3b59d187aec
sxb <xiaobaistc@sina.com>
2023-04-01 10:51:31 +0800

    sxb

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [vue()],
-  base:'./',
+  base:'/mousemove/', // './'
   assetsPublicPath:'./',
   resolve: {
     'alias': {
